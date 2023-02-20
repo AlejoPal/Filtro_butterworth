dc4ce7779223680f9ea62e8536cabf51a540b799
AlejoPal <alejandro.palacios.apf2000@gmail.com>
2023-02-19 19:20:57 -0500

    correcion errores imagenes dobles al calcular

--- Java/MostrarinfoBajos.js
+++ Java/MostrarinfoBajos.js
@@ -36,38 +36,47 @@ function Mostrarinfo(){
     console.log(orden);
     switch(orden){
         case 2: 
+            document.getElementById('Imagen').innerHTML = "";
             document.getElementById("Imagen").appendChild(imgPasabajos2);
             genera_tabla(calculo2Orden());
             break;
         case 3:
+            document.getElementById('Imagen').innerHTML = "";
             document.getElementById("Imagen").appendChild(imgPasabajos3);
             genera_tabla(calculo2Orden());
             break;
         case 4:
+            document.getElementById('Imagen').innerHTML = "";
             document.getElementById("Imagen").appendChild(imgPasabajos4);
             genera_tabla(calculo4Orden());
             break;
         case 5:
+            document.getElementById('Imagen').innerHTML = "";
             document.getElementById("Imagen").appendChild(imgPasabajos5);
             genera_tabla(calculo5Orden());
             break;
         case 6:
+            document.getElementById('Imagen').innerHTML = "";
             document.getElementById("Imagen").appendChild(imgPasabajos6);
             genera_tabla(calculo6Orden());
             break;
         case 7:
+            document.getElementById('Imagen').innerHTML = "";
             document.getElementById("Imagen").appendChild(imgPasabajos7);
             genera_tabla(calculo7Orden());
             break;
         case 8:
+            document.getElementById('Imagen').innerHTML = "";
             document.getElementById("Imagen").appendChild(imgPasabajos8);
             genera_tabla(calculo8Orden());
             break;
         case 9:
+            document.getElementById('Imagen').innerHTML = "";
             document.getElementById("Imagen").appendChild(imgPasabajos9);
             genera_tabla(calculo9Orden());
             break; 
         case 10:
+            document.getElementById('Imagen').innerHTML = "";
             document.getElementById("Imagen").appendChild(imgPasabajos10);
             genera_tabla(calculo10Orden());
             break;   
